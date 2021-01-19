0e6442cceba5c2a41f4f2f637515e65031c5cb4c
oopzzozzo <ek3ru8m4@gmail.com>
2021-01-19 08:32:52 +0800

    fast key auto commit

--- insert.js
+++ insert.js
@@ -1,11 +1,9 @@
 window.onload=function(){
-  /*
   elems = document.getElementsByTagName("code");
   fetch("https://raw.githubusercontent.com/oopzzozzo/sql-cheat/master/bucket.sql")
     .then(response => response.text())
     .then((code) => {
       elems[0].innerHTML = code;
     });
-    */
   PR.prettyPrint();
 }
